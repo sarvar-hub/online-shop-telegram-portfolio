0b9e514bd1869528ad6db829ed4d5abfab253d9d
Sarvar Xudoyberdiyev <sxweb20@gmail.com>
2024-02-27 15:54:49 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import './App.css'
+const tg = window.Telegram.WebApp;
+
+function App() {
+
+  useEffect(()=> {
+    tg.ready(); 
+  }, [])
+
+  const onClose = () => {
+    tg.close();
+  }
+  
+  return (
+    <>
+      <h1 className="text-">Hello World with TailwindCSS</h1>
+      <button onClick={onClose}>Закрый</button>
+    </>
+  )
+}
+
+export default App
