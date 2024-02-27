138c4d3da597522dd0a81dd4e4aa758a254d0248
Sarvar Xudoyberdiyev <sxweb20@gmail.com>
2024-02-27 16:23:40 +0500

    added tailwind and absolute import

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,22 @@
 import { useEffect } from 'react';
-import './App.css'
+import '@/App.css'
 const tg = window.Telegram.WebApp;
 
 function App() {
 
-  useEffect(()=> {
-    tg.ready(); 
+  useEffect(() => {
+    tg.ready();
   }, [])
 
   const onClose = () => {
     tg.close();
   }
-  
+
   return (
-    <>
-      <h1 style={{color: 'green'}}>Hello World with TailwindCSS</h1>
-      <button onClick={onClose}>Закрый</button>
-    </>
+    <div className="flex flex-wrap">
+      <h1 className="text-3xl font-bold underline mr-5">Hello World with TailwindCSS</h1>
+      <button className="bg-green-500 text-white px-2 py-1" onClick={onClose}>Закрый</button>
+    </div>
   )
 }
 
